25a9c53da9a53d017a4dc8d66a271135c9cb996c
John Buswell <buswellj@gmail.com>
2016-08-25 01:36:57 -0400

    Added more comments to the code

--- fibsync/index.js
+++ fibsync/index.js
@@ -20,7 +20,7 @@ app.use(morgan('combined', {stream: accessLogStream}));
 // API Endpoints
 
 app.get('/', function (req, res) {
- res.status(200).send('Request Received from ' + req.ip);
+ res.status(200).send('<a href="/api/1/generate/5">Use /api/1/generate/X</a> where X is a value from 0 to 1476.<br />Request Received from ' + req.ip);
 });
 
 app.get('/api/:apiver/', function (req, res) {
@@ -34,17 +34,25 @@ app.get('/api/:apiver/', function (req, res) {
 app.get('/api/:apiver/generate/:fibn/', function (req, res) {
  var fibn = req.params.fibn;
 
+ // First we check the API version number
+ // We check the value is an actual number
+ // We check the value is less than zero for negative number case
+ // We check the value is less than 1477 (prevent abuse / exceeded type bounds)
  if (req.params.apiver != '1') {
   res.status(404).send('Error: Unsupported API version. Use version 1');
- } else if (fibn < '0') {
-  res.status(500).send('Error: Value cannot be a negative number');
  } else if (isNaN(fibn)) {
   res.status(500).send('Error: Value must be a number');
+ } else if (fibn < '0') {
+  res.status(500).send('Error: Value cannot be a negative number');
+ } else if (fibn > 1477) {
+  res.status(403).send('Forbidden: Value must be less than 1477');
  } else {
   fscalc(req,res,fibn);
  }
 });
 
+// This function places the application on port 3000
+// Throws some information on the console
 var server = app.listen(3000, function () {
 
  var host = server.address().address;
@@ -57,6 +65,11 @@ var server = app.listen(3000, function () {
  console.log('Listening on http://%s:%s', host, port);
  console.log('');
 });
+
+// This function does basic fibonacci sequence generation
+// Returns the values in JSON format using:
+// { id: X, result: Y } where id is the sequence id, and result is the result
+// capture special cases for first two values
 
 var fscalc = function(req, res, fibn) {
  var fsn = 1;
